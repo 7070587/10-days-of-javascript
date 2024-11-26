bcb9c4e5f3545ecfe9d2ebb0b2a22c1798114448
TinaLin.Lin <TinaLin.Lin@advantech.com.tw>
2024-11-26 09:46:03 +0800

    Add | Day 3

---
+++ day-3/arrays.js
@@ -0,0 +1,9 @@
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+function getSecondLargest(nums) {
+  // Complete the function
+  return Array.from(new Set(nums)).sort((a, b) => b - a)[1];
+}
